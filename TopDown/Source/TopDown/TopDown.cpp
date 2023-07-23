556dfa85d4c9e50382aaedd54f128cbcc39f9c46
moad6127 <moad6127@gmail.com>
2023-07-23 16:57:18 +0900

    Feat : TopDown게임
    TopDown게임 프로젝트 만들고 캐릭터 메쉬를 이주 시키기

---
+++ TopDown/Source/TopDown/TopDown.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TopDown.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TopDown, "TopDown" );
---
+++ TopDown/Source/TopDown/TopDownGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TopDownGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOPDOWN_API ATopDownGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
