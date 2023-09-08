3464943675a1b96862b2abb9248bb978f01cf158
moad6127 <moad6127@gmail.com>
2023-09-08 17:45:07 +0900

    Feat : TopDown게임
    SaveGame클래스를 C++로 만들기

---
+++ TopDown/Source/TopDown/Private/SaveGame/TopDownSaveGame.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SaveGame/TopDownSaveGame.h"
+
+UTopDownSaveGame::UTopDownSaveGame()
+{
+	Gold = 0;
+	PlayerName = TEXT("Guest");
+}
---
+++ TopDown/Source/TopDown/Public/SaveGame/TopDownSaveGame.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "TopDownSaveGame.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOPDOWN_API UTopDownSaveGame : public USaveGame
+{
+	GENERATED_BODY()
+public:
+
+	UTopDownSaveGame();
+
+	UPROPERTY()
+	int32 Gold;
+
+	FString PlayerName;
+};
