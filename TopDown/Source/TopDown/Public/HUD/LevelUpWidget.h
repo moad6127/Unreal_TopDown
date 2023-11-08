16bd813bc7b46dec27e511d167b7395bdf69fead
moad6127 <moad6127@gmail.com>
2023-11-08 17:48:36 +0900

    Feat : TopDown게임
    위젯 을 C++로 만들어두기

---
+++ TopDown/Source/TopDown/Public/HUD/LevelUpWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "LevelUpWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOPDOWN_API ULevelUpWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
